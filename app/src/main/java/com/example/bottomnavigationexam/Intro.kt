f4310ea95b201e63e8f477979ad127b8b4e88abf
sm1374 <sm1374@naver.com>
2020-11-02 09:38:00 +0900

    바텀네비게이션, 네비게이션 뷰, 프래그먼트에서 인텐트까지

---
+++ app/src/main/java/com/example/bottomnavigationexam/Intro.kt
@@ -0,0 +1,30 @@
+package com.example.bottomnavigationexam
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import androidx.core.app.ActivityCompat
+
+class Intro : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_intro)
+
+        var handler = Handler()
+        handler.postDelayed({
+            var intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        },3000)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        ActivityCompat.finishAffinity(this)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        ActivityCompat.finishAffinity(this)
+    }
+}
---
+++ app/src/main/java/com/example/bottomnavigationexam/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.example.bottomnavigationexam
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Observer
+import androidx.navigation.NavController
+import androidx.navigation.ui.NavigationUI.setupActionBarWithNavController
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var bottomNavigationView: BottomNavigationView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        if(savedInstanceState == null){
+            setUpBottomNavigationBar()
+        }
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        setUpBottomNavigationBar()
+    }
+
+    private fun setUpBottomNavigationBar(){
+        bottomNavigationView = findViewById(R.id.bottom_nav)
+        val navGraphIds = listOf(R.navigation.navigation_home, R.navigation.navigation_hot, R.navigation.navigation_list)
+
+        bottomNavigationView.setupWithNavController(
+            navGraphIds, supportFragmentManager, R.id.nav_host_container, intent
+        )
+    }
+}
