e330e00a6b1003020815948e65c3b90bf58591bc
sm1374 <sm1374@naver.com>
2020-11-04 19:06:57 +0900

    바텀메뉴숨기기

--- app/src/main/java/com/example/bottomnavigationexam/HomeDetailFragment.kt
+++ app/src/main/java/com/example/bottomnavigationexam/HomeDetailFragment.kt
@@ -7,5 +7,18 @@ import android.view.View
 import android.view.ViewGroup
 
 class HomeDetailFragment : Fragment(R.layout.fragment_home_detail) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
 
+        val mainAct = activity as MainActivity
+        mainAct.hideBottomNavi(true)
+    }
+
+    // 안해주면 뒤로가기 해도 바텀메뉴가 안돌아오기 때문에 해줘야됨
+    override fun onDestroy() {
+        super.onDestroy()
+
+        val mainAct = activity as MainActivity
+        mainAct.hideBottomNavi(false)
+    }
 }
--- app/src/main/java/com/example/bottomnavigationexam/MainActivity.kt
+++ app/src/main/java/com/example/bottomnavigationexam/MainActivity.kt
@@ -3,12 +3,14 @@ package com.example.bottomnavigationexam
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.Observer
 import androidx.navigation.NavController
 import androidx.navigation.ui.NavigationUI.setupActionBarWithNavController
 import androidx.navigation.ui.setupActionBarWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     private lateinit var bottomNavigationView: BottomNavigationView
@@ -34,6 +36,13 @@ class MainActivity : AppCompatActivity() {
         bottomNavigationView.setupWithNavController(
             navGraphIds, supportFragmentManager, R.id.nav_host_container, intent
         )
+    }
+    fun hideBottomNavi(state : Boolean) {
+        if(state){
+            bottom_nav.visibility = View.GONE
+        }else {
+            bottom_nav.visibility = View.VISIBLE
+        }
     }
 
     // 뒤로가기 누를시 앱종료
