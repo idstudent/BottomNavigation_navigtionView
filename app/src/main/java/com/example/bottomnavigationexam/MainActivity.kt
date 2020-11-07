ffdbfcd55d6fb779f36aa50b7651c0e7f59d11bb
sm1374 <sm1374@naver.com>
2020-11-07 13:40:22 +0900

    네비게이션드로어

--- app/src/main/java/com/example/bottomnavigationexam/MainActivity.kt
+++ app/src/main/java/com/example/bottomnavigationexam/MainActivity.kt
@@ -3,27 +3,55 @@ package com.example.bottomnavigationexam
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.Gravity
+import android.view.MenuItem
 import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.GravityCompat
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.Observer
 import androidx.navigation.NavController
 import androidx.navigation.ui.NavigationUI.setupActionBarWithNavController
 import androidx.navigation.ui.setupActionBarWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationView
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
     private lateinit var bottomNavigationView: BottomNavigationView
+    lateinit var toggle : ActionBarDrawerToggle
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        test.setOnClickListener {
+            drawerLayout.openDrawer(GravityCompat.START)
+        }
+        navView.setNavigationItemSelectedListener(this)
 
         if(savedInstanceState == null){
             setUpBottomNavigationBar()
         }
     }
 
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.miIteml -> Toast.makeText(applicationContext, "click 1", Toast.LENGTH_SHORT).show()
+            R.id.miItem2 -> Toast.makeText(applicationContext, "click 2", Toast.LENGTH_SHORT).show()
+            R.id.miItem3 -> Toast.makeText(applicationContext, "click 3", Toast.LENGTH_SHORT).show()
+        }
+        drawerLayout.closeDrawers()
+        return false
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(toggle.onOptionsItemSelected(item)) {
+
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
     override fun onRestoreInstanceState(savedInstanceState: Bundle) {
         super.onRestoreInstanceState(savedInstanceState)
         setUpBottomNavigationBar()
@@ -45,10 +73,14 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    // 뒤로가기 누를시 앱종료
-//    override fun onBackPressed() {
-//        super.onBackPressed()
-//        Log.e("tag", "back")
-//        finish()
-//    }
+    override fun onBackPressed() {
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawers()
+            Log.e("tag", "1")
+        }else {
+            super.onBackPressed()
+            Log.e("tag", "2")
+        }
+    }
+
 }
